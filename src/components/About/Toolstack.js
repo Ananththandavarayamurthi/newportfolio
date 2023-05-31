6d5977384dc5ceaaa1b63be8d8eca63f9cd2f286
Ananth <110927979+Ananththandavarayamurthi@users.noreply.github.com>
2023-05-31 23:48:07 +0530

    Update Toolstack.js
    new tool stack

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -6,27 +6,47 @@ import {
   SiNetlify,
   SiHeroku,
   SiGithub,
+  SiPostman,
+  SiRender
 } from "react-icons/si";
 
 function Toolstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiVisualstudiocode />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiFigma />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiGithub />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiHeroku />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiNetlify />
-      </Col>
-    </Row>
+    <Col xs={4} md={2} className="tech-icons">
+      <SiVisualstudiocode />
+      <h3 style={{ size: "20px" }} >Visual studio</h3>
+    </Col>
+    <Col xs={4} md={2} className="tech-icons">
+      <SiGithub />
+      <h3 style={{ size: "20px" }} >GitHub</h3>
+    </Col>
+    <Col xs={4} md={2} className="tech-icons">
+      <SiPostman />
+      <h3 style={{ size: "20px" }} >Postman</h3>
+    </Col>
+    <Col xs={4} md={2} className="tech-icons">
+      <SiFigma />
+      <h3 style={{ size: "20px" }} >Figma</h3>
+    </Col>
+    
+    <Col xs={4} md={2} className="tech-icons">
+      <SiNetlify />
+      <h3 style={{ size: "20px" }} >Netlify</h3>
+    </Col>
+    <Col xs={4} md={2} className="tech-icons">
+      <SiRender />
+      <h3 style={{ size: "20px" }} >Render</h3>
+    </Col>
+    
+    
+    <Col xs={4} md={2} className="tech-icons">
+      <SiHeroku />
+      <h3 style={{ size: "20px" }} >Heroku</h3>
+    </Col>
+    
+  </Row>
+    
   );
 }
 
